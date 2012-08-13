a2f3834502c3b3540757c7af5dd4e06f459cb435
Christophe <christophe.belpaire@gmail.com>
2012-08-13 20:58:12 +0200

    first commit

---
+++ public/sticker.js
@@ -0,0 +1,49 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "generatedBy"   : "Blender 2.63 Exporter",
+        "vertices"      : 4,
+        "faces"         : 2,
+        "normals"       : 1,
+        "colors"        : 0,
+        "uvs"           : 4,
+        "materials"     : 1,
+        "morphTargets"  : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "material_0.002",
+	"blending" : "NormalBlending",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"depthTest" : true,
+	"depthWrite" : true,
+	"shading" : "Lambert",
+	"specularCoef" : 50,
+	"transparency" : 1.0,
+	"transparent" : false,
+	"vertexColors" : false
+	}],
+
+    "vertices": [0.791719,-0.800000,0.899201,0.791719,0.800000,0.899201,-0.808281,0.800000,0.899201,-0.808281,-0.800000,0.899201],
+
+    "morphTargets": [],
+
+    "normals": [0.000000,0.000000,1.000000],
+
+    "colors": [],
+
+    "uvs": [[1.000000,1.000000,1.000000,0.000000,0.000000,0.000000,0.000000,1.000000]],
+
+    "faces": [42,0,1,2,0,0,1,2,0,0,0,42,0,2,3,0,0,2,3,0,0,0]
+
+
+
+}
